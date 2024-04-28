cd15a3a263dc16693a00512e6f2abe821d3513cd
arturbartonelli <arturbartonelli@gmail.com>
2024-04-28 15:47:30 -0300

    Adicionando Angular Material e criando elemento header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,17 +3,21 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HeaderComponent } from './components/template/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
