cd8cd6fb7a68407555fc3e3bd5d222e6c0c727ff
arturbartonelli <arturbartonelli@gmail.com>
2024-04-28 15:10:59 -0300

    Colocando no-standlone

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   template: `
     <h1>Welcome to {{title}}!</h1>
 
     <router-outlet />
   `,
-  styles: [],
+  styles: []
 })
 export class AppComponent {
   title = 'frontend';
