94542b969186da38009e63624ded278643a6ab26
arturbartonelli <arturbartonelli@gmail.com>
2024-04-30 17:00:34 -0300

    Home e aprendendo sobre diretivas

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,23 +7,33 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { HeaderComponent } from './components/template/header/header.component';
 import { NavComponent } from './components/template/nav/nav.component';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { RedDirective } from './directives/red.directive';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule} from '@angular/material/list';
+import { ForDirective } from './directives/for.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    ProductCrudComponent,
+    RedDirective,
+    ForDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
     MatListModule,
+    MatCardModule,
     MatSidenavModule
   ],
   providers: [
